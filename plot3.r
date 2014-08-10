7ad38de4e6cd9b5ba6df84f961c8ddf860c5b140
DavidS <llp_yyz@hotmail.com>
2014-08-10 09:41:09 -0700

    Forgot to add updated plot/r file that uses the png() fcn call.

--- plot3.r
+++ plot3.r
@@ -21,10 +21,10 @@ sm3 <- pd_subset$Sub_metering_3
 sm <- c(sm1, sm2, sm3)
 g <- gl(3, length(sm1), labels = c("sm1","sm2","sm3"))
 x <- c(pd_subset$Date,pd_subset$Date,pd_subset$Date)
+png(filename = 'plot3.png', width = 480, height = 480, units = 'px')
 plot(x,sm, xlab = "", ylab = "Energy sub metering", type = "n") #create plot region with everything except the data
 lines(x[g == "sm1"], sm[g == "sm1"], col = "black") #add points for sub metering 1
 lines(x[g == "sm2"], sm[g == "sm2"], col = "red") #add points for sub metering 2
 lines(x[g == "sm3"], sm[g == "sm3"], col = "blue") #add points for sub metering 3
 legend("topright", lty = "solid",  col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-dev.copy(png, file = "plot3.png")
 dev.off()
